e20826c2925e36e3f25923005eaa242bf13c1d1b
Adrian Fisher <amtfisher96@gmail.com>
2026-02-07 15:39:25 -0600

    Succesfully creates FITS files on osdf

--- download_fits.py
+++ download_fits.py
@@ -2,13 +2,14 @@ import os
 import timescape_functions as tf
 import sys
 from pathlib import Path
+import subprocess
 
 csv_file = sys.argv[1]
 process = int(sys.argv[2])
-fits_dir = sys.argv[3]
+fits_dir = Path(sys.argv[3])  # convert to Path
 
-# Always make a local directory
-Path(fits_dir).mkdir(parents=True, exist_ok=True)
+# Always make a local directory first
+fits_dir.mkdir(parents=True, exist_ok=True)
 
 CHUNK = 50
 TOTAL_ROWS = 377294
@@ -17,6 +18,9 @@ start = process * CHUNK
 end   = min((process + 1) * CHUNK, TOTAL_ROWS)
 
 try:
+    print(f'Writing {end-start} FITS files to {fits_dir}... ')
     tf.download_fits_chunk(csv_file, start, end, fits_dir)
 except Exception as e:
     print(f'Submission failed: {e}\n {start} until {end}')
+
+
