8bdfa75cdbd1db20a4d70492626016fbd467ca74
Adrian Fisher <amtfisher96@gmail.com>
2026-02-11 00:25:24 -0600

    Still failing to create pickle files

--- run_create_pickle_files.sh
+++ run_create_pickle_files.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-JOB_LINE=$1
-SDSS_CSV=$2
-OUT_FOLDER=$3
-PROCESS=$4
+JOB_LINE=$2           
+SDSS_CSV=$3           
+OUT_FOLDER=$4         
+PROCESS=$5
 
+# Make a folder inside the job scratch to hold pickle files
 mkdir -p "$OUT_FOLDER"
 
-python3 create_pickle_files.py "$JOB_LINE" "$SDSS_CSV" "$OUT_FOLDER" "$PROCESS"
+# Run Python to create the pickle files
+python3  create_pickle_files.py "$JOB_LINE" "$SDSS_CSV" "$OUT_FOLDER" "$PROCESS"
