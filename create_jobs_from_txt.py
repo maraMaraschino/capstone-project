b1b1f8c24f82a40e6f2011cb26b0f46c22b70299
Adrian F <amtfisher96@gmail.com>
2026-02-11 10:05:53 -0700

    Updating create_jobs_from_txt

--- create_jobs_from_txt.py
+++ create_jobs_from_txt.py
@@ -3,7 +3,7 @@ import sys
 
 valid_fits_csv_file = sys.argv[1]
 job_txt_file = sys.argv[2]
-chunk_size = 1000
+chunk_size = 150
 
 def convert_csv_to_txt(valid_fits_csv_file, job_txt_file, chunk_size):
     df = pd.read_csv(valid_fits_csv_file, header=0)
@@ -14,6 +14,6 @@ def convert_csv_to_txt(valid_fits_csv_file, job_txt_file, chunk_size):
         for i in range(0, len(paths), chunk_size):
             batch = paths[i:i+chunk_size]
             full_paths = [osdf_prefix + name for name in batch]
-            f.write(" ".join(full_paths) + "\n")
+            f.write(",".join(full_paths) + "\n")
 
 convert_csv_to_txt(valid_fits_csv_file, job_txt_file, chunk_size)
